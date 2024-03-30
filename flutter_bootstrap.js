f3eddf388c14de9fe79e6c9820e250007c3da024
EricApostal <ehapostal04@gmail.com>
2024-03-30 15:27:29 -0400

    remove typewriter text

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"3588d31a98f6c2a1949132e1cf610265b04185
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1228923822"
+    serviceWorkerVersion: "1506165568"
   }
 });
